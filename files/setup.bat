8c26449cfb97c43e311c88ccbae88c6c15972c29
yukineko <hideki030403@gmail.com>
2020-07-29 07:19:14 +0900

    Ver 1.0.4

--- files/setup.bat
+++ files/setup.bat
@@ -7,16 +7,19 @@ set defaultDir=%PROGRAMFILES%\HSChecker
 
 cls
 
-echo モバイルホットスポット有効化セットアップ Ver1.0.3
+echo モバイルホットスポット有効化セットアップ Ver1.0.4
 echo.
 echo 1. ファイルのダウンロード
 timeout 3
 
 mkdir "%defaultDir%"
 bitsadmin /transfer download %filehost%run.vbs "%defaultDir%\run.vbs"
+echo ダウンロードに成功しました。
+
+cls
 
 echo 2. 自動実行タスクの作成
-
+echo.
 schtasks /create /tn HSChecker /tr "'wscript.exe' '%defaultDir%\run.vbs'" /sc minute /mo 10 /rl highest /F
 echo タスクの作成に成功しました。
 
