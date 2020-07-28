85b0b1f053eb8b2d48ec35c980cd7e6e806c158f
yukineko <hideki030403@gmail.com>
2020-07-29 00:40:09 +0900

    Ver 1.0.0

--- files/run.vbs
+++ files/run.vbs
@@ -1,4 +1,6 @@
-Dim oShell
-Set oShell = WScript.CreateObject ("WSCript.shell")
-oShell.run "check.bat",0
-Set oShell = Nothing
+Set Ws = WScript.CreateObject("WScript.Shell")
+HSstatus = Ws.RegRead("HKLM\SOFTWARE\Microsoft\PolicyManager\current\device\WiFi\AllowInternetSharing")
+
+If HSstatus = 0 Then
+    Ws.RegWrite "HKLM\SOFTWARE\Microsoft\PolicyManager\current\device\WiFi\AllowInternetSharing", 1, "REG_DWORD"
+End If
--- files/setup.bat
+++ files/setup.bat
@@ -3,14 +3,22 @@ setlocal
 
 : Config
 set filehost=https://github.com/hideki0403/monooki/raw/master/files/
+set defaultDir=%PROGRAMFILES%\HSChecker
 
 cls
 
 echo モバイルホットスポット有効化セットアップ Ver1.0.0
-echo 
+echo.
 echo 1. ファイルのダウンロード
 timeout 3
 
-mkdir "%PROGRAMFILES%\HSChecker"
-bitsadmin /transfer download %filehost%check.bat "%PROGRAMFILES%\HSChecker\check.bat"
-bitsadmin /transfer download %filehost%run.vbs "%PROGRAMFILES%\HSChecker\run.vbs"
+mkdir "%defaultDir%"
+bitsadmin /transfer download %filehost%run.vbs "%defaultDir%\HSChecker\run.vbs"
+
+echo 2. 自動実行タスクの作成
+
+schtasks /create /tn HSChecker /tr "'wcsript.exe' '%defaultDir%\HSChecker\run.vbs'" /sc minute /mo 10 /rl highest /F
+echo タスクの作成に成功しました。
+
+echo.
+echo セットアップが完了しました。
