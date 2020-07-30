3f211c1ab4975191f9b946f9d22d532599accf51
yukineko <hideki030403@gmail.com>
2020-07-30 14:03:24 +0900

    Ver 1.1.0

--- files/setup.bat
+++ files/setup.bat
@@ -7,7 +7,7 @@ set defaultDir=%PROGRAMFILES%\HSChecker
 
 cls
 
-echo モバイルホットスポット有効化セットアップ Ver1.0.4
+echo モバイルホットスポット有効化セットアップ Ver1.1.0
 echo.
 echo 1. ファイルのダウンロード
 timeout 3
@@ -22,6 +22,20 @@ echo 2. 自動実行タスクの作成
 echo.
 schtasks /create /tn HSChecker /tr "'wscript.exe' '%defaultDir%\run.vbs'" /sc minute /mo 10 /rl highest /F
 echo タスクの作成に成功しました。
+echo.
+echo 3. タスク設定の変更
+
+schtasks /query /tn HSChecker /xml > hsc.xml
+setlocal enabledelayedexpansion
+for /f "delims=" %%a in (hsc.xml) do (
+  set str=%%a
+  echo !str:^<DisallowStartIfOnBatteries^>true=^<DisallowStartIfOnBatteries^>false! >> temp.xml
+)
+
+endlocal
+schtasks /create /tn HSChecker /xml temp.xml /f
+del temp.xml
+del hsc.xml
 
 echo.
 echo セットアップが完了しました。
