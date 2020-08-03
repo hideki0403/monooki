d6375d5aa953ae320e7054fd99b4d7e2be247161
yukineko <hideki030403@gmail.com>
2020-08-03 17:50:06 +0900

    Delete sc.bat

--- files/sc.bat
+++
@@ -1,9 +0,0 @@
-@echo off
- 
-sc query state=inactive | findstr BluetoothUserService_*
-
-if %errorlevel% == 0 (
-    cd %PROGRAMFILES%\HSChecker
-    devcon remove =Bluetooth @USB*
-    devcon rescan
-)
